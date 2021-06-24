e5bafe1095e0b51deba3183a7bb3c50f9e762e94
Valeria <lerik13@gmail.com>
2021-06-24 13:34:54 -0600

    OOP - use store object in state.js

--- src/index.js
+++ src/index.js
@@ -4,20 +4,19 @@ import './index.css';
 import App from './App';
 //import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
-import state from './redux/state';
-import {addPost, updateNewPostText, subscribe} from './redux/state';
+import store from './redux/state';
 import { BrowserRouter } from 'react-router-dom';
 
 let rerenderEntireTree = (state) => {
 	ReactDOM.render(
 		<BrowserRouter>
-			<App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+			<App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)} />
 		</BrowserRouter>, document.getElementById('root')
 	);
 }
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,68 +1,74 @@
-let rerenderEntireTree = (state) => {
-	// заглушка = plug
-	console.log('State was changed');
-}
-
-let state = {
-	profilePage: {
-		posts: [
-			{id: 1, message: 'Hi, how are you!', likesCount: 12},
-			{id: 2, message: 'It is my first post', likesCount: 5},
-			{id: 3, message: 'Bla-bla', likesCount: 1},
-			{id: 4, message: 'da-da', likesCount: 0}
-		],
-		newPostText: 'new post'
+// store - OOP
+let store = {
+	_state: {
+		profilePage: {
+			posts: [
+				{id: 1, message: 'Hi, how are you!', likesCount: 12},
+				{id: 2, message: 'It is my first post', likesCount: 5},
+				{id: 3, message: 'Bla-bla', likesCount: 1},
+				{id: 4, message: 'da-da', likesCount: 0}
+			],
+			newPostText: 'new post'
+		},
+		dialogsPage: {
+			dialogs: [
+				{id: 1, name: 'Dima'},
+				{id: 2, name: 'Anna'},
+				{id: 3, name: 'Ivan'},
+				{id: 4, name: 'Viktor'},
+				{id: 5, name: 'Olya'},
+				{id: 6, name: 'Andrew'},
+			],
+			messages: [
+				{id: 1, message: 'Hi!'},
+				{id: 2, message: 'How are you?'},
+				{id: 3, message: 'Yo-o-o-o'},
+				{id: 4, message: 'Yo'},
+				{id: 5, message: 'What\'s up'},
+				{id: 6, message: 'Hello'},
+			]
+		},
+		sidebar: {
+			friends: [
+				{id: 1, name: 'Kolya'},
+				{id: 2, name: 'Nastya'},
+				{id: 3, name: 'Yana'},
+				{id: 4, name: 'Stanislav'},
+				{id: 5, name: 'Vova'},
+			]
+		}
 	},
-	dialogsPage: {
-		dialogs: [
-			{id: 1, name: 'Dima'},
-			{id: 2, name: 'Anna'},
-			{id: 3, name: 'Ivan'},
-			{id: 4, name: 'Viktor'},
-			{id: 5, name: 'Olya'},
-			{id: 6, name: 'Andrew'},
-		],
-		messages: [
-			{id: 1, message: 'Hi!'},
-			{id: 2, message: 'How are you?'},
-			{id: 3, message: 'Yo-o-o-o'},
-			{id: 4, message: 'Yo'},
-			{id: 5, message: 'What\'s up'},
-			{id: 6, message: 'Hello'},
-		]
+	getState() {
+		return this._state;
 	},
-	sidebar: {
-		friends: [
-			{id: 1, name: 'Kolya'},
-			{id: 2, name: 'Nastya'},
-			{id: 3, name: 'Yana'},
-			{id: 4, name: 'Stanislav'},
-			{id: 5, name: 'Vova'},
-		]
+
+	_callSubscriber() {
+		// заглушка = plug
+		console.log('State was changed');
+	},
+	subscribe(observer) {
+		this._callSubscriber = observer;
+	},
+
+	addPost() {
+		//debugger;
+		let newPost = {
+			id: 5,
+			message: this._state.profilePage.newPostText,
+			likesCount: 0
+		}
+		this._state.profilePage.posts.push(newPost);
+		this._state.profilePage.newPostText = '';
+		this._callSubscriber(this._state);
+	},
+	
+	// Синхронизация данных, при каждом изменении -> меняется State
+	updateNewPostText(newText) {
+		this._state.profilePage.newPostText = newText;
+		this._callSubscriber(this._state);
 	}
+
 }
 
-window.state = state;
-
-export const addPost = () => {
-	//debugger;
-	let newPost = {
-		id: 5,
-		message: state.profilePage.newPostText,
-		likesCount: 0
-	}
-	state.profilePage.posts.push(newPost);
-	state.profilePage.newPostText = '';
-	rerenderEntireTree(state);
-}
-// Синхронизация данных, при каждом изменении -> меняется State
-export const updateNewPostText = (newText) => {
-	state.profilePage.newPostText = newText;
-	rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-	rerenderEntireTree = observer;
-}
-
-export default state;
+export default store;
+window.store = store;
